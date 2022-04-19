f5ff4647721eb11c9296199ac10d11e1be42ba01
yuyi <419940108@qq.com>
2022-04-20 01:16:52 +0800

    初始化项目

---
+++ src/zkSync.Automation/Models/AccountModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zkSync.Automation.Models
+{
+    public class AccountModel
+    {
+        public string Address { get; set; }
+        public string PrivateKey { get; set; }
+        public string Mnemonic { get; set; }
+    }
+}
